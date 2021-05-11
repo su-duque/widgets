c8fbd08fb0f17e08bda1d8655d11dc272b8e4d99
Susana Duque <su.duquem@gmail.com>
2021-05-10 22:44:49 -0500

    Se añade la funcionalidad de abrir los links del Header con Ctrl/Cmd + Click

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,17 @@ import React from 'react';
 const Link = ({ href, className, children }) => {
   const onClick = (event) => {
     // Helper function - Event handler
+
+    /* Para conservar el comportamiento de que al presionar Ctrl y se
+    le dé click a uno de los links del Header se abra una nueva pestaña
+    con lo que tiene el link, se configura el siguiente if */
+    if(event.metaKey || event.ctrlKey){
+      // event.metaKey: Mac OS y event.ctrlKey: Windows - son bolean properties
+      return;
+      /* Si alguna de las keys es true, se retornará early
+      y no se ejecutarán las siguientes líneas de código */
+    }
+
     //Para evitar que la página se recargue cada que se haga click en los links:
     event.preventDefault(); //Previene el comportamiento normal del navegador
     // Función del navegador para controlar la URL:
